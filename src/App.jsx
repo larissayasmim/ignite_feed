180ce3515a28a953aa13a2d39eececc31c42df41
Larissa Yasmim <yasmimlarissa09@gmail.com>
2023-02-13 15:26:24 -0300

    Adiciona sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import { Header } from './componentes/header';
 import { Post }  from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './componentes/Sidebar';
+
 
 export function App() {
   return (
     <div>
       <Header />
+      
+      <div className={styles.wrapper}>
 
-      <Post 
-        author="Larissa" 
-        content="lorem" 
-      />
+        <Sidebar />
+        <main>
+            <Post 
+              author="Larissa" 
+              content="lorem" 
+            />
 
-      <Post 
-        author="Min" 
-        content="hbcdvuefsdbfhheffj"
-      />
+            <Post 
+              author="Min" 
+              content="hbcdvuefsdbfhheffjtestando"
+            />
 
-      <Post />
-      <Post />
+        </main>
+      </div>  
     </div>
   )
 }
---
+++ src/componentes/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine} from "phosphor-react";
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return(
+            <aside className={styles.sidebar} >
+                <img 
+                    className={styles.cover} 
+                    src="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
+                />
+
+                <div className={styles.profile} >
+                    <img 
+                    className={styles.avatar}
+                    src="https://media.licdn.com/dms/image/C4D03AQERmfk_iV-dgg/profile-displayphoto-shrink_800_800/0/1587567528318?e=1681948800&v=beta&t=_hTJxYXZrY-SawPRhSMuLgYL-3vpgw1-TZBqGevcJtQ" 
+                    />
+                    <strong>Larissa Yamim</strong>
+                    <span>Web Developer</span>
+                </div> 
+
+                <footer>
+                    <a href="#">
+                    <PencilLine size={20}/>  
+                        Editar seu Perfil
+                    </a>
+                </footer>
+
+            </aside>
+    );
+}
